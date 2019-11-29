3d3f2b001bd29aca136949656413edfcfc6fb190
Amumuwa <mm178250@163.com>
2019-11-29 19:00:34 +0800

    update：登陆页校验

--- client/config-overrides.js
+++ client/config-overrides.js
@@ -1,4 +1,6 @@
-const { override, fixBabelImports, addLessLoader } = require('customize-cra');
+// 引入需要的模块
+const { override, fixBabelImports, addLessLoader, addWebpackAlias } = require('customize-cra');
+const path = require('path')
 
 module.exports =override (
     // 针对 antd 实现按需打包：根据 import 的内容进行打包（使用 babel-plugin-import）
@@ -11,6 +13,13 @@ module.exports =override (
     // 使用 less-loader 对源码 antd 中的 less 变量重新赋值
     addLessLoader({
         javascriptEnabled: true,
-        modifyVars: { '@primary-color': '#1DA57A' } // 自定义全局基色（主题）
+        modifyVars: { '@primary-color': '#01144A' } // 自定义全局基色（主题）
+    }),
+
+    // 配置路径别名
+    addWebpackAlias({
+        'styles': path.join(__dirname, './src/styles'),
+        '@': path.join(__dirname, './src/components'),
+        'imgs': path.join(__dirname, './src/imgs')
     })
 )
--- client/src/views/login/login.jsx
+++ client/src/views/login/login.jsx
@@ -1,11 +1,114 @@
-// 登陆路由界面
-
+// 登陆界面
 import React, { Component } from 'react'
 
-export default class Login extends Component {
+import 'styles/login/login.less'
+import logo from 'imgs/GM.png'
+
+import { Form, Icon, Input, Button } from 'antd';
+
+// const Item = Form.Item  必须写在 import 之后
+
+class Login extends Component {
+    handleSubmit = (e) => {
+        // 阻止表单自动提交
+        e.preventDefault()  
+
+        // 点击提交时进行校验
+        this.props.form.validateFields(
+            (err, values) => {
+                if (!err) {
+                    console.log(values)
+                    // 校验通过
+                } else {
+                    // 校验失败
+                }
+            }
+        )
+
+        const form = this.props.form    // form 对象 
+
+        // 获取 Form 中所有 Item 的 value，获取到一个对象，对象的 key 是给 Item 传入的名称
+        const formValues = form.getFieldsValue()
+    }
+
+    // 自定义密码校验
+    validatePwd = (rule, value, callback) => {
+        if (!value) {
+            callback ('请输入密码！') // 验证失败，传入的参数是失败时提示的文本
+        } else if (value.length < 7){
+            callback ('密码至少七位') 
+        }else if (value.length > 16){
+            callback ('密码最多十六位') 
+        }else if (!/^[a-zA-Z0-9_]+$/.test(value)){
+            callback ('密码必须是英文、数字、下划线组成！') 
+        } else {
+            callback () // 验证通过
+        }    
+    }
+
     render () {
-        return <div>
-            Login
+        const form = this.props.form    // Form(Login) 向 Form 组件传递的对象
+        const { getFieldDecorator } = form
+
+        return <div className="login">
+            <header className="hedaer">
+                <div className="hedaer-content">
+                    <img className="logo" src={logo} alt="logo" />
+                    <h1 className="title">Mcould</h1>
+                </div>               
+            </header>
+
+            <section className="content">
+                <h2>用户登陆</h2>               
+                <Form className="login-form" onSubmit={ this.handleSubmit }>
+                    <Form.Item>
+                        {
+                            getFieldDecorator ('username', {
+                                // 表单校验，声明式验证，直接使用定义好的规则进行验证
+                                rules: [ 
+                                    { required: true, whitespace: true, message: '请输入用户名！' }, 
+                                    { min: 2, message: '用户名最少两位！'},
+                                    { max: 10, message: '用户名最多十位！'},
+                                    { pattern: /^[a-zA-Z0-9_]+$/, message: '用户名必须是英文、数字、下划线组成！'}
+                                ]
+                            }) (
+                                <Input
+                                    prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />}
+                                    placeholder="用户名"
+                                />
+                            )
+                        }
+                    </Form.Item>
+
+                    <Form.Item>
+                        {
+                            getFieldDecorator ('password', {
+                                rules: [ 
+                                    { validator: this.validatePwd }
+                                ]
+                            }) (
+                                <Input
+                                    prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />}
+                                    type="password"
+                                    placeholder="密码"
+                                />
+                            )
+                        }                       
+                    </Form.Item>
+
+                    <Form.Item>
+                        <Button type="primary" htmlType="submit" className="login-form-button">
+                            登陆
+                        </Button>
+                    </Form.Item>
+                </Form>                
+            </section>
         </div>
     }
 }
+
+// 包装 Form 组件（内部有 Form 表单的组件），生成一个新组件 Form(Login)
+// 新组件会向 Form 组件传递一个对象属性： form
+const LoginForm = Form.create()(Login)
+
+export default LoginForm
